8a7ca72c610abb497791333fb898e3c2f4f3b089
Kevin Stine <ikaikastine@gmail.com>
2015-11-16 08:39:44 -0800

    Added more game options

--- GTW/globalthermonuclearwar.c
+++ GTW/globalthermonuclearwar.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main () {
-	char *play = "SHALL WE PLAY A GAME?\n TIC-TAC-TOE";
+	char *play = "SHALL WE PLAY A GAME?\n TIC-TAC-TOE\n BLACK JACK\n HEARTS\n BRIDGE\n CHECKERS\n CHESS\n POKER\n GLOBAL THERMONUCLEAR WAR\n";
 
 	int i = 0;
 
