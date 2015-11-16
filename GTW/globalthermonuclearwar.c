f3dbf227979fc954f4c168e5e3e57a1ad211af81
Kevin Stine <ikaikastine@gmail.com>
2015-11-16 08:44:15 -0800

    Added user prompt

--- GTW/globalthermonuclearwar.c
+++ GTW/globalthermonuclearwar.c
@@ -5,13 +5,15 @@ int main () {
 	char *play = "SHALL WE PLAY A GAME?\n TIC-TAC-TOE\n BLACK JACK\n HEARTS\n BRIDGE\n CHECKERS\n CHESS\n POKER\n GLOBAL THERMONUCLEAR WAR\n";
 
 	int i = 0;
-
+	char str [80];
 	while(play[i] != '\0') {
 		putchar(play[i]);
 		fflush(stdout);
 		usleep(100000);
 		i++;
 	}
+
+	scanf("%s", str);
 
 	return 0;
 }
